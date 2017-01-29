ba6dade91c2e9be4dd52de990c7caa0250f7ae74
PCSailor <Phil@PhilCurtis.io>
2017-01-29 08:50:32 -0600

    InitialCommit_script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log(new Date().getFullYear() + " Javascript Code test");
+// PA_PreWork_Wk-05_Github-Object Assessment
+// Philip Curtis
+// Phi Cohort
+avengersMovie = {
+    name: "Avengers",
+    runtime: 143 + "mins",
+    releaseYear: 2012,
+    director: "Joss Whedon",
+  }
+console.log(avengersMovie);
